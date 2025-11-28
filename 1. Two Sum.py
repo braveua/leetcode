5c201eb39d4b72e25f8fca8c753391aad0dc8c75
braveua <braveua@gmail.com>
2025-11-28 10:59:47 +0200

    #1 complete

--- 1. Two Sum.py
+++ 1. Two Sum.py
@@ -5,3 +5,12 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """ 
+        for i,num in enumerate(nums):
+            if target - num in nums[i + 1 :]:
+                x=nums.index(target - num,i+1)
+                return (i,x)
+            
+
+c = Solution()
+res = c.twoSum([2,7,11,15],9)
+print(res)
